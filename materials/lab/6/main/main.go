218b31edfefb1224f6a9ad926daa49df2b629ed8
Danny Radosevich <deradosevich@gmail.com>
2022-04-01 17:35:54 -0600

    Lab6 done

--- materials/lab/6/main/main.go
+++ materials/lab/6/main/main.go
@@ -3,35 +3,66 @@ package main
 // main.go HAS FOUR TODOS - TODO_1 - TODO_4
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 	"scrape/scrape"
+
+	"bufio"
+	"os"
+
+	"github.com/gorilla/mux"
 )
 
-//TODO_1: Logging right now just happens, create a global constant integer LOG_LEVEL 
+//TODO_1: Logging right now just happens, create a global constant integer LOG_LEVEL
 //TODO_1: When LOG_LEVEL = 0 DO NOT LOG anything
-//TODO_1: When LOG_LEVEL = 1 LOG API details only 
+//TODO_1: When LOG_LEVEL = 1 LOG API details only
 //TODO_1: When LOG_LEVEL = 2 LOG API details and file matches (e.g., everything)
 
 func main() {
-	
-	log.Println("starting API server")
+	fmt.Println("When LOG_LEVEL = 0 DO NOT LOG anything")
+	fmt.Println("When LOG_LEVEL = 1 LOG API details only")
+	fmt.Println("When LOG_LEVEL = 2 LOG API details and file matches (e.g., everything)")
+
+	//prompt for user input
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please select a log number based on the above options: ")
+	text, _ := reader.ReadString('\n')
+
+	fmt.Print(text)
+	if text == "0\n" {
+		scrape.Log_lvl = 0
+		fmt.Println("Logging set to level 0\n\tWhen LOG_LEVEL = 0 DO NOT LOG anything")
+	} else if text == "1\n" {
+		scrape.Log_lvl = 1
+		fmt.Println("Logging set to level 1\n\tWhen LOG_LEVEL = 1 LOG API details only")
+	} else if text == "2\n" {
+		scrape.Log_lvl = 2
+		fmt.Println("Logging set to level 2\n\t2 LOG API details and file matches (e.g., everything)")
+	} else {
+		fmt.Println("NO valid level inputed, defaulting to 0")
+		scrape.Log_lvl = 0
+	}
+	if scrape.Log_lvl > 0 {
+		log.Println("starting API server")
+	}
+
 	//create a new router
 	router := mux.NewRouter()
-	log.Println("creating routes")
+	if scrape.Log_lvl > 0 {
+		log.Println("creating routes")
+	}
+
 	//specify endpoints
 	router.HandleFunc("/", scrape.MainPage).Methods("GET")
 
 	router.HandleFunc("/api-status", scrape.APISTATUS).Methods("GET")
 
 	router.HandleFunc("/indexer", scrape.IndexFiles).Methods("GET")
-	router.HandleFunc("/search", scrape.FindFile).Methods("GET")		
-    //TODO_2 router.HandleFunc("/addsearch/{regex}", scrape.TODOREPLACE).Methods("GET")
-    //TODO_3 router.HandleFunc("/clear", scrape.TODOREPLACE).Methods("GET")
-    //TODO_4 router.HandleFunc("/reset", scrape.TODOREPLACE).Methods("GET")
-
-
+	router.HandleFunc("/search", scrape.FindFile).Methods("GET")
+	router.HandleFunc("/addsearch/{regex}", scrape.AddRegEx).Methods("GET")
+	router.HandleFunc("/clear", scrape.ClearRegEx).Methods("GET")
+	router.HandleFunc("/reset", scrape.ResetRegEx).Methods("GET")
 
 	http.Handle("/", router)
 
